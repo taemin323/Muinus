44566966c6a40d39897fa3efafcc5875a693b8e9
aseongjun99 <aseongjun99@gmail.com>
2025-01-26 15:31:16 +0900

    [FEAT] 카카오 로그인 인가 코드 및 액세스 토큰 발급 구현

--- Backend/src/main/java/com/hexa/muinus/users/controller/OauthController.java
+++ Backend/src/main/java/com/hexa/muinus/users/controller/OauthController.java
@@ -1,4 +1,22 @@
 package com.hexa.muinus.users.controller;
 
+import com.hexa.muinus.users.service.OauthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class OauthController {
+
+    private final OauthService oauthService;
+
+    @GetMapping("/api/users/login")
+    public ResponseEntity<?> kakaoLogin(@RequestParam("code") String authorizationCode) {
+        oauthService.requestAccessTokenFromKakao(authorizationCode);
+        return ResponseEntity.ok().build();
+    }
 }
--- Backend/src/main/java/com/hexa/muinus/users/service/OauthService.java
+++ Backend/src/main/java/com/hexa/muinus/users/service/OauthService.java
@@ -1,4 +1,57 @@
 package com.hexa.muinus.users.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@Service
 public class OauthService {
+
+    @Value("${kakao.auth.client_id}")
+    private String clientId;
+
+    @Value("${kakao.auth.redirect_url}")
+    private String redirectUrl;
+
+    public String requestAccessTokenFromKakao(String authorizationCode) {
+        log.info("requestAccessTokenFromKakao");
+
+        // 카카오로 액세스 토큰 받기 위한 객체 생성
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("grant_type", "authorization_code");
+        body.add("client_id", clientId);
+        body.add("redirect_uri", redirectUrl);
+        body.add("code", authorizationCode);
+
+        HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<>(body, headers);
+
+        // 카카오로 액세스 토큰 발급 요청
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.exchange(
+                "https://kauth.kakao.com/oauth/token",
+                HttpMethod.POST,
+                httpEntity,
+                String.class
+        );
+
+        // 반환 값에서 액세스 토큰 추출
+        ObjectMapper objectMapper = new ObjectMapper();
+        String accessToken = "";
+        try {
+            accessToken = objectMapper.readTree(responseEntity.getBody()).get("access_token").asText();
+            log.info("Access token:" + accessToken);
+        } catch (Exception e) {
+            log.error("카카오 액세스 토큰 교환 오류 발생");
+        }
+        return accessToken;
+    }
 }
