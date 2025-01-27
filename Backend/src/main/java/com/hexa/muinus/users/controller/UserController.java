e943a9404e856f4541703202cb22f5a0e6205e36
aseongjun99 <aseongjun99@gmail.com>
2025-01-27 22:23:25 +0900

    [FEAT] 소비자 회원가입 구현

--- Backend/src/main/java/com/hexa/muinus/users/controller/UserController.java
+++ Backend/src/main/java/com/hexa/muinus/users/controller/UserController.java
@@ -1,4 +1,27 @@
 package com.hexa.muinus.users.controller;
 
+import com.hexa.muinus.users.dto.ConsumerRegisterRequestDto;
+import com.hexa.muinus.users.dto.StoreOwnerRegisterRequestDto;
+import com.hexa.muinus.users.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/api/users/consumer")
+    public ResponseEntity<?> registerConsumer(@RequestBody ConsumerRegisterRequestDto requestDto) {
+        return ResponseEntity.ok(userService.registerConsumer(requestDto));
+    }
+
+    @PostMapping("/api/users/store-owner")
+    public ResponseEntity<?> registerStoreOwner(@RequestBody StoreOwnerRegisterRequestDto requestDto) {
+
+    }
 }
--- Backend/src/main/java/com/hexa/muinus/users/service/UserService.java
+++ Backend/src/main/java/com/hexa/muinus/users/service/UserService.java
@@ -1,4 +1,25 @@
 package com.hexa.muinus.users.service;
 
+import com.hexa.muinus.users.UserRepository;
+import com.hexa.muinus.users.domain.user.Users;
+import com.hexa.muinus.users.dto.ConsumerRegisterRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public Users registerConsumer(ConsumerRegisterRequestDto requestDto) {
+        Users user = Users.builder()
+                .userName(requestDto.getUserName())
+                .email(requestDto.getUserEmail())
+                .telephone(requestDto.getUserTelephone())
+                .userType(Users.UserType.U)
+                .point(requestDto.getUserPoint())
+                .build();
+        return userRepository.save(user);
+    }
 }
