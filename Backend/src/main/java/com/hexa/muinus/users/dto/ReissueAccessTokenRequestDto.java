b4d4d6a75de4b8207b3e1542528d94e9bf131f4a
aseongjun99 <aseongjun99@gmail.com>
2025-01-30 23:44:40 +0900

    [FEAT] RefreshToken으로 AccessToken 재발급 기능 구현

---
+++ Backend/src/main/java/com/hexa/muinus/users/dto/ReissueAccessTokenRequestDto.java
@@ -0,0 +1,11 @@
+package com.hexa.muinus.users.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReissueAccessTokenRequestDto {
+
+    private String userEmail;
+}
--- Backend/src/main/java/com/hexa/muinus/users/service/UserService.java
+++ Backend/src/main/java/com/hexa/muinus/users/service/UserService.java
@@ -5,7 +5,9 @@ import com.hexa.muinus.store.repository.StoreRepository;
 import com.hexa.muinus.users.UserRepository;
 import com.hexa.muinus.users.domain.user.Users;
 import com.hexa.muinus.users.dto.ConsumerRegisterRequestDto;
+import com.hexa.muinus.users.dto.ReissueAccessTokenRequestDto;
 import com.hexa.muinus.users.dto.StoreOwnerRegisterRequestDto;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,6 +25,14 @@ public class UserService {
 
     @Transactional
     public Users registerConsumer(ConsumerRegisterRequestDto requestDto, HttpServletResponse response) {
+        // 이미 가입한 회원인지 이메일로 확인
+        if (userRepository.findByEmail(requestDto.getUserEmail()) != null) {
+
+            log.info("User already exists with email {}", requestDto.getUserEmail());
+            return null;
+            // 에러 처리
+        }
+
         Users user = Users.builder()
                 .userName(requestDto.getUserName())
                 .email(requestDto.getUserEmail())
@@ -39,11 +49,14 @@ public class UserService {
 
     /**
      * 점주 회원가입 시 정보 받아와 Users 테이블에 저장, 또한 Store 테이블에도 저장
-     * @param requestDto
-     * @return
      */
     @Transactional
     public int registerStoreOwner(StoreOwnerRegisterRequestDto requestDto, HttpServletResponse response) {
+        // 이미 가입한 회원인지 이메일로 확인
+        if (userRepository.findByEmail(requestDto.getUserEmail()) != null) {
+            // 에러 처리
+        }
+
         Users user = Users.builder()
                 .userName(requestDto.getUserName())
                 .email(requestDto.getUserEmail())
@@ -76,6 +89,25 @@ public class UserService {
         String accessToken = jwtProvider.createAccessToken(user);
         String refreshToken = jwtProvider.createRefreshToken(user);
         user.updateRefreshToken(refreshToken);
-        jwtProvider.setTokensInCookie(response, accessToken, refreshToken);
+        jwtProvider.setAccessTokensInCookie(response, accessToken);
+        jwtProvider.setRefreshTokensInCookie(response, refreshToken);
+    }
+
+    /**
+     * refresh token 유효성 검증 후 access token 재발급
+     */
+    @Transactional
+    public void reissueAccessToken(HttpServletRequest request, HttpServletResponse response, ReissueAccessTokenRequestDto requestDto) {
+        String refreshToken = jwtProvider.getCookieValue(request, "RefreshToken");
+
+        // RefreshToken 유효한지 확인
+        if (refreshToken != null && !refreshToken.isEmpty() && jwtProvider.validateToken(refreshToken)) {
+                // DB에 저장된 refresh 토큰과 일치하는지 확인 후 AccessToken 재발급
+                Users user = userRepository.findByEmail(requestDto.getUserEmail());
+                if (refreshToken.equals(user.getEmail())) {
+                    String accessToken = jwtProvider.createAccessToken(user);
+                    jwtProvider.setAccessTokensInCookie(response, accessToken);
+                }
+        }
     }
 }
