b23f74ebdf013fecd06eb3a5f320a354e5d26a33
aseongjun99 <aseongjun99@gmail.com>
2025-02-02 00:31:23 +0900

    [REFACTOR] 플리마켓 상품 담기 수정

--- Backend/src/main/java/com/hexa/muinus/store/domain/item/repository/FliItemRepository.java
+++ Backend/src/main/java/com/hexa/muinus/store/domain/item/repository/FliItemRepository.java
@@ -1,8 +1,8 @@
 package com.hexa.muinus.store.domain.item.repository;
 
 import com.hexa.muinus.store.domain.item.FliItem;
+import com.hexa.muinus.store.domain.store.Store;
 import com.hexa.muinus.store.dto.FliItemDTO;
-import com.hexa.muinus.store.dto.PutFliItemResponseDTO;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -22,6 +22,5 @@ public interface FliItemRepository extends CrudRepository<FliItem, Integer> {
 """)
     List<FliItemDTO> findSellingFliItemsByStore(@Param("storeNo") int storeNo);
 
-    @Query(value = "SELECT fli_item_name, price FROM fli_item WHERE store_no = :storeNo and section_id = :sectionId", nativeQuery = true)
-    Optional<PutFliItemResponseDTO> findFliItemByStoreNoAndSectionId(@Param("storeNo") Integer storeNo, @Param("sectionId") Integer sectionId);
+    Optional<FliItem> findByStoreAndSectionId(Store store, Integer sectionId);
 }
