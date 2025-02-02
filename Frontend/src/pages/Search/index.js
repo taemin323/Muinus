d72eeab032585d764332c4c5f769cf2ecf33946e
unknown <bazzi456@naver.com>
2025-02-02 16:30:46 +0900

    frontendshlee

---
+++ Frontend/src/pages/Search/index.js
@@ -0,0 +1,18 @@
+import HeaderContainer from "../../components/HeaderContainer";
+import SearchBar from "../../components/"
+
+function Search() {
+
+
+    return (
+        <div>
+            <HeaderContainer />
+            <SearchBar />
+            <DraggableBottomSheet />
+            <BottomNav />
+        </div>
+    );
+}
+
+
+export default Search;
