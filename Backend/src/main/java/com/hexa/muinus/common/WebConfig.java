0abb909d67b9246a9e1115fbab3fc59e8aecb671
aseongjun99 <aseongjun99@gmail.com>
2025-02-15 13:43:24 +0900

    [FIX] CORS 테스트

--- Backend/src/main/java/com/hexa/muinus/common/WebConfig.java
+++ Backend/src/main/java/com/hexa/muinus/common/WebConfig.java
@@ -20,8 +20,9 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000", "https://i12a506.p.ssafy.io")
-                .allowedMethods("*")
-                .allowedHeaders("Content-Type", "Accept", "Origin", "X-Requested-With")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("*")
                 .allowCredentials(true);
     }
 
