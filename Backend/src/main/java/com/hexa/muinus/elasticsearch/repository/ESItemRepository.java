af2034d3a7055c7d98876ad20edd71ea61a6ac75
이지희 <chaehee13@naver.com>
2025-02-18 01:41:27 +0900

    [BE/FEAT/#58] 미사용쿼리 삭제

--- Backend/src/main/java/com/hexa/muinus/elasticsearch/repository/ESItemRepository.java
+++ Backend/src/main/java/com/hexa/muinus/elasticsearch/repository/ESItemRepository.java
@@ -12,29 +12,4 @@ public interface ESItemRepository extends ElasticsearchRepository<ESItem, Intege
 
     List<ESItem> findByItemNameStartingWith(String prefix);
 
-    @Query("""
-    {
-       "bool": {
-        "should": [
-          {"match": {"item_name.synonym_graph": {"query": "?0", "fuzziness": "2", "boost": 0.8, "operator": "OR"}}},
-          {"match": {"item_name.nori": {"query": "?0", "fuzziness": "2", "boost": 1.2, "operator": "OR"}}},
-          {"match": {"item_name.ngram": {"query": "?0", "fuzziness": "2", "boost": 1, "operator": "OR"}}},
-          {"match": {"item_name.nori_shingle": {"query": "?0", "fuzziness": "2", "boost": 1.8, "operator": "OR"}}},
-          {"match": {"item_name.ngram_shingle": {"query": "?0", "fuzziness": "2", "boost": 1.5, "operator": "OR"}}}
-        ],
-       "filter": [
-         { "range": { "sugars": { "gte": ?1, "lte": ?2 } } },
-         { "range": { "calories": { "gte": ?3, "lte": ?4 } } }
-       ]
-      }
-    }
-    """)
-    List<ESItem> searchItemsByQuery(String query, Integer minSugar, Integer maxSugar, Integer minCal, Integer maxCal);
-
-    @Query(
-    """
-                   
-    """
-    )
-    List<ESItem> getNoriSearchTokens(String query);
 }
